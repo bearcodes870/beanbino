4572b3da8541b385c389bdccd92e68f008f1db18
Bear <bear.ranasinghe@gmail.com>
2021-02-09 23:15:04 -0500

    routing for login and signup work

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 import SignupPage from '../SignupPage/SignupPage';
+import LoginPage from '../LoginPage/LoginPage';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <Link className='btn btn-default' to='/signup'>Signup</Link>
       <Route exact path='/signup' render={() =>
         <SignupPage
+        
+        />
+      } />
+      <Link className='btn btn-default' to='/login'>Login</Link>
+      <Route exact path='/login' render={() =>
+        <LoginPage
         
         />
       } />
---
+++ src/pages/LoginPage/LoginPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LoginPage = (props) => {
+
+  return (
+    <p>This is your login page</p>
+  );
+
+};
+
+export default LoginPage;
