6d8daa24788fafc9a788a73dd25366cb1306d270
Bear <bear.ranasinghe@gmail.com>
2021-02-13 11:23:50 -0500

    touch tokenService

--- models/user.js
+++ models/user.js
@@ -1,7 +1,35 @@
 const mongoose = require('mongoose');
-// optional shortcut to the mongoose.Schema class
+const bcrypt = require('bcrypt');
+const SALT_ROUNDS = 6;
+
+
 const userSchema = new mongoose.Schema({
-    username: String,
-})
+  name: String,
+  email: {type: String, required: true, lowercase: true, unique: true},
+  password: String
+}, {
+  timestamps: true
+});
+
+userSchema.set('toJSON', {
+  transform: function(doc, ret) {
+    // remove the password property when serializing doc to JSON
+    delete ret.password;
+    return ret;
+  }
+});
+
+userSchema.pre('save', function(next) {
+    // this will be set to the current document
+    const user = this;
+    if (!user.isModified('password')) return next();
+    // password has been changed - salt and hash it
+    bcrypt.hash(user.password, SALT_ROUNDS, function(err, hash) {
+    if (err) return next(err);
+    // replace the user provided password with the hash
+    user.password = hash;
+    next();
+  });
+});
 
 module.exports = mongoose.model('User', userSchema);
